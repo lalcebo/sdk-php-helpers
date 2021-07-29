5695dd9a8c0500f2b569d2e5995dcfd72df11725
Jorge P. Hernandez Lalcebo <lalcebo2003@gmail.com>
2021-07-29 12:33:03 -0400

    Refactored arr class.

--- tests/ArrTest.php
+++ tests/ArrTest.php
@@ -23,7 +23,12 @@ class ArrTest extends TestCase
         'name' => [
             'first' => 'Bowen',
             'last' => 'Peters',
-            'age' => 40
+            'age' => 40,
+            'car' => [
+                'brand' => 'Audi',
+                'color' => 'Black',
+                'miles' => 1000
+            ]
         ],
         'phone' => '+1 (989) 554-2724',
         'registered' => 'Friday, August 2, 2019 7 =>51 PM',
@@ -42,6 +47,9 @@ class ArrTest extends TestCase
         'name_first' => 'Bowen',
         'name_last' => 'Peters',
         'name_age' => 40,
+        'name_car_brand' => 'Audi',
+        'name_car_color' => 'Black',
+        'name_car_miles' => 1000,
         'phone' => '+1 (989) 554-2724',
         'registered' => 'Friday, August 2, 2019 7 =>51 PM',
         'tags_0' => 'reprehenderit',
@@ -53,14 +61,83 @@ class ArrTest extends TestCase
     public function toFlattenResult(): void
     {
         $toFlat = Arr::flatten($this->providedTestArray);
+        self::assertEquals(
+            [
+                '605f6f26a6a97004210e0c99',
+                4,
+                'dc03ae65-e057-4270-a912-db15e4f5f029',
+                'Bowen',
+                'Peters',
+                40,
+                'Audi',
+                'Black',
+                1000,
+                '+1 (989) 554-2724',
+                'Friday, August 2, 2019 7 =>51 PM',
+                'reprehenderit',
+                'incididunt',
+                'eu'
+            ],
+            $toFlat
+        );
+    }
+
+    /** @test */
+    public function toFlattenWithKeysResult(): void
+    {
+        $toFlat = Arr::flattenWithKeys($this->providedTestArray);
         self::assertEquals($this->providedFlattenedArray, $toFlat);
     }
 
-    /** @test  */
-    public function toMultidimensionalResult(): void
+    /** @test */
+    public function flattenWithKeysAndDepthResult(): void
     {
-        $toMultiDimensional = Arr::multiDimensional($this->providedFlattenedArray);
-        self::assertEquals($this->providedTestArray, $toMultiDimensional);
+        $toFlat1 = Arr::flattenWithKeys($this->providedTestArray, 0);
+        self::assertEquals(
+            [
+                'id' => '605f6f26a6a97004210e0c99',
+                'index' => 4,
+                'guid' => 'dc03ae65-e057-4270-a912-db15e4f5f029',
+                'phone' => '+1 (989) 554-2724',
+                'registered' => 'Friday, August 2, 2019 7 =>51 PM'
+            ],
+            $toFlat1
+        );
+
+        $toFlat2 = Arr::flattenWithKeys($this->providedTestArray, 1);
+        self::assertEquals(
+            [
+                'id' => '605f6f26a6a97004210e0c99',
+                'index' => 4,
+                'guid' => 'dc03ae65-e057-4270-a912-db15e4f5f029',
+                'name_first' => 'Bowen',
+                'name_last' => 'Peters',
+                'name_age' => 40,
+                'phone' => '+1 (989) 554-2724',
+                'registered' => 'Friday, August 2, 2019 7 =>51 PM',
+                'tags_0' => 'reprehenderit',
+                'tags_1' => 'incididunt',
+                'tags_2' => 'eu'
+            ],
+            $toFlat2
+        );
+    }
+
+    /** @test */
+    public function flattenToMultiDimensionalResult(): void
+    {
+        $toMultiDimensional1 = Arr::dimensional($this->providedFlattenedArray);
+        self::assertEquals($this->providedTestArray, $toMultiDimensional1);
+
+        $toMultiDimensional2 = Arr::dimensional(
+            [
+                '605f6f26a6a97004210e0c99',
+                4,
+                '2_0' => 'Audi',
+                '2_1' => 1000
+            ]
+        );
+        self::assertEquals(['605f6f26a6a97004210e0c99', 4, ['Audi', 1000]], $toMultiDimensional2);
     }
 
     /** @test */
@@ -83,6 +160,6 @@ class ArrTest extends TestCase
             }
         );
 
-        self::assertEquals(['index' => 4, 'name' => ['age' => 40]], $filter2);
+        self::assertEquals(['index' => 4, 'name' => ['age' => 40, 'car' => ['miles' => 1000]]], $filter2);
     }
 }
